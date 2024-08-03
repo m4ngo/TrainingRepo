fdf3e7fc445692563b1f0e43fa3f2c736aeaf68b
m4ngo <arik4231@gmail.com>
2024-08-03 10:04:31 -0700

    Add new autonomous program for training

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleop.java
@@ -1,33 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Robot: MyTeleop", group="Robot")
-@Disabled
 public class MyTeleop extends LinearOpMode {
 
     /* Declare OpMode members. */
-    public DcMotor thisIsAMotor = null;
+    public DcMotor left_drive = null;
+    public DcMotor right_drive = null;
+
     public Servo thisIsAServo = null;
 
-    public double motorControl = 0.0;
     public double servoControl = 0.5;
 
     public static double SERVO_INIT_POS = 0.5;
-    public static double SERVO_SPEED = 0.01;
+    public static double SERVO_SPEED = 0.05;
 
     @Override
     public void runOpMode() {
 
         // Define and Initialize Motors
-        thisIsAMotor = hardwareMap.get(DcMotor.class, "motorName");
-        thisIsAMotor.setDirection(DcMotor.Direction.FORWARD);
-        // thisIsAMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left_drive = hardwareMap.get(DcMotor.class, "left_drive");
+        right_drive = hardwareMap.get(DcMotor.class, "right_drive");
+
+        left_drive.setDirection(DcMotor.Direction.FORWARD);
+//         thisIsAMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Define and initialize ALL installed servos.
         thisIsAServo = hardwareMap.get(Servo.class, "servoName");
@@ -44,10 +44,15 @@ public class MyTeleop extends LinearOpMode {
         while (opModeIsActive()) {
 
             //Gamepad input is built into LinearOpMode
-            motorControl = gamepad1.right_stick_y;
-            thisIsAMotor.setPower(motorControl);
+            left_drive.setPower(gamepad1.right_stick_y);
+            right_drive.setPower(gamepad1.left_stick_y);
 
-            servoControl += gamepad1.left_stick_y * SERVO_SPEED;
+            if(gamepad1.dpad_up){
+                servoControl += SERVO_SPEED;
+            }
+            if(gamepad1.dpad_down){
+                servoControl -= SERVO_SPEED;
+            }
             //clamp the servo position between 0.0 and 1.0
             if(servoControl > 1.0) {
                 servoControl = 1.0;
@@ -58,7 +63,7 @@ public class MyTeleop extends LinearOpMode {
             thisIsAServo.setPosition(servoControl);
 
             // Send telemetry message to signify robot running;
-            telemetry.addData("motorControl",  "%.2f", motorControl);
+            telemetry.addData("servoControl",  "%.2f", servoControl);
             telemetry.update();
 
             // Pace this loop so it run at a reasonable speed.
